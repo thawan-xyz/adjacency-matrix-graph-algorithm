5e65b9d317deda46069e5306511e4907180013e3
thawan-xyz <thawan.xyz@gmail.com>
2024-06-22 23:14:10 -0300

    update

--- main.cpp
+++ main.cpp
@@ -2,86 +2,104 @@
 #include <vector>
 using namespace std;
 
-class AdjacencyMatrixGraph {
+class Adjacency_Matrix_Graph {
 private:
-    const int vertices;
+    const int VERTICES;
     int edges;
     vector<vector<int>> matrix;
-    vector<bool> mark;
+    vector<bool> marked;
 
-public:
-    AdjacencyMatrixGraph(const int vertices) : vertices(vertices), edges(0) {
-        matrix.assign(vertices, vector<int>(vertices, 0));
-        mark.assign(vertices, false);
+    void depth_first_search(int v) {
     }
 
-    ~AdjacencyMatrixGraph() = default;
+    void breadth_first_search(int v) {
+    }
 
-    void setEdge(const int v, const int w, const int x) {
+public:
+    Adjacency_Matrix_Graph(const int n) : VERTICES(n), edges(0) {
+        matrix.assign(VERTICES, vector<int>(VERTICES, 0));
+        marked.assign(VERTICES, false);
+    }
+
+    ~Adjacency_Matrix_Graph() = default;
+
+    void set_edge(const int v, const int w, const int x) {
         if (matrix[v][w] == 0) {
             edges++;
         }
         matrix[v][w] = x;
     }
 
-    void delEdge(const int v, const int w) {
+    void del_edge(const int v, const int w) {
         if (matrix[v][w] != 0) {
             edges--;
         }
         matrix[v][w] = 0;
     }
 
-    bool hasEdge(int v, int w) const {
+    bool has_edge(int v, int w) const {
         if (matrix[v][w] != 0) {
             return true;
         }
         return false;
     }
 
-    int getWeight(const int v, const int w) const {
+    int get_weight(const int v, const int w) const {
         return matrix[v][w];
     }
 
-    int getAdjacent(const int v) const {
-        for (int i = 0; i < vertices; ++i) {
-            if (matrix[v][i] != 0) {
-                return i;
+    int get_adjacent(const int v) const {
+        for (int w = 0; w < VERTICES; ++w) {
+            if (matrix[v][w] != 0) {
+                return w;
             }
         }
-        return vertices;
+        return VERTICES;
     }
 
-    int getNext(const int v, const int p) const {
-        for (int i = p + 1; i < vertices; ++i) {
-            if (matrix[v][i] != 0) {
-                return i;
+    int get_next(const int v, const int p) const {
+        for (int w = p + 1; w < VERTICES; ++w) {
+            if (matrix[v][w] != 0) {
+                return w;
             }
         }
-        return vertices;
+        return VERTICES;
     }
 
-    void setMark(const int v, const bool m) {
-        mark[v] = m;
+    void set_mark(const int v, const bool m) {
+        marked[v] = m;
     }
 
-    bool getMark(const int v) const {
-        return mark[v];
+    bool get_mark(const int v) const {
+        return marked[v];
     }
 
-    int numVertices() const {
-        return vertices;
+    int num_vertices() const {
+        return VERTICES;
     }
 
-    int numEdges() const {
+    int num_edges() const {
         return edges;
     }
 
-    void DFS() {
+    void dfs_traversal() {
+        marked.assign(VERTICES, false);
 
+        for (int v = 0; v < VERTICES; ++v) {
+            if (get_mark(v) == false) {
+                depth_first_search(v);
+            }
+        }
     }
 
-    void BFS() {
+    void bfs_traversal() {
+        marked.assign(VERTICES, false);
 
+        for (int v = 0; v < VERTICES; ++v) {
+            if (get_mark(v) == false) {
+                breadth_first_search(v);
+            }
+        }
     }
 };
 
